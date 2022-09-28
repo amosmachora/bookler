1dc3640dfd0618e635ec5fb03ac71fad1a3a07a4
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-09-28 21:20:27 +0300

    Init

---
+++ src/Assets/Assets.js
@@ -0,0 +1,11 @@
+import Cloud_1 from "./image.png";
+import Cloud_2 from "./image_1.png";
+import Cloud_3 from "./image_2.png";
+import Plane from "./Object.png";
+
+export default {
+  Cloud_1,
+  Cloud_2,
+  Cloud_3,
+  Plane,
+};
