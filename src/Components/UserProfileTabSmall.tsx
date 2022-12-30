09ea3ebf1bed7e7abe5e30e9c2611451360ec0dc
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-12-30 14:40:54 +0300

    Added log out button

--- src/Components/UserProfileTabSmall.tsx
+++ src/Components/UserProfileTabSmall.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Assets } from "../Assets/Assets";
 import { AuthProvider } from "./Contexts/AuthenticationProvider";
 
@@ -9,15 +9,21 @@ const UserProfileTabSmall = ({
   setMenuWide: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const { userData } = useContext(AuthProvider);
+  const navigate = useNavigate();
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+
+  const handleLogOut = () => {
+    localStorage.removeItem("userData");
+    navigate("/login");
+  };
 
   return (
-    <>
-      <Link
-        className="ml-5 rounded-full bg-white/40 backdrop-blur-lg w-44 pr-4 pl-1 pt-1 pb-1 text-white flex items-center justify-between cursor-pointer"
+    <div className="ml-5">
+      <div
+        className="rounded-full bg-white/40 backdrop-blur-lg w-44 pr-4 pl-1 pt-1 pb-1 text-white flex items-center justify-between cursor-pointer"
         onClick={() => {
-          setMenuWide(false);
+          setShowOptions((prev) => !prev);
         }}
-        to="profile"
       >
         <img
           src={
@@ -28,8 +34,29 @@ const UserProfileTabSmall = ({
         />
         <p className="text-xs">{userData.name}</p>
         <img src={Assets.DropDown} alt="Drop down" />
-      </Link>
-    </>
+      </div>
+      {showOptions && (
+        <div className="bg-white mt-3 rounded-md py-2">
+          <Link
+            className="ml-2 text-xs flex items-center cursor-pointer justify-center py-1"
+            onClick={() => {
+              setMenuWide(false);
+              setShowOptions(false);
+            }}
+            to="profile"
+          >
+            <p className="text-blue-600 font-semibold">Profile</p>
+          </Link>
+          <div
+            className="ml-2 text-xs flex items-center cursor-pointer justify-center py-1 mt-2"
+            onClick={handleLogOut}
+          >
+            <p className="text-red-600 font-semibold">Log out </p>
+            <img src={Assets.LogOut} alt="Log out" className="h-4 w-4 ml-1" />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
